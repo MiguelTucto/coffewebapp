466049be596f50c2229342e69a59e90cb6cd60a5
MiguelTucto <u201521691@upc.edu.pe>
2024-05-28 18:10:28 -0500

    fix: data type problems v2

--- src/sections/form.tsx
+++ src/sections/form.tsx
@@ -4,7 +4,7 @@ import FirstChocolateProduct from "../../public/images/firstchocolateproduct.jpg
 import SecondChocolateProduct from "../../public/images/secondchocolateproduct.jpg";
 import ThirdChocolateProduct from "../../public/images/thirdchocolateproduct.jpg";
 import Image from "next/image";
-import { useForm } from 'react-hook-form';
+import {SubmitHandler, useForm} from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { useEffect, useState } from 'react';
@@ -38,7 +38,7 @@ const Form = () => {
         setIsFormValid(isValid);
     }, [formValues]);
 
-    const onSubmit = async (data: typeof formData) => {
+    const onSubmit: SubmitHandler<FormData> = async (data) => {
         setIsLoading(true);
         const formData = new FormData();
         for (const key in data) {
@@ -57,8 +57,8 @@ const Form = () => {
 
     return (
         <>
-            <div className={'grid grid-cols-2 md:px-[200px] px-[50px] md:pt-[200px] pt-6 pb-20'}>
-                <div className={'md:block hidden'}>
+            <div className="grid grid-cols-2 md:px-[200px] px-[50px] md:pt-[200px] pt-6 pb-20 bg-gradient-to-r from-yellow-100 to-yellow-200">
+                <div className="hidden md:block">
                     <div className="grid grid-cols-3 gap-1">
                         <div className="col-span-2 ">
                             <Image src={'https://i.postimg.cc/yYLfDXv0/firstchocolateproduct.jpg'} width={1000}
@@ -98,9 +98,9 @@ const Form = () => {
                     </div>
                 </div>
 
-                <div className={'md:ml-20 ml-0 md:col-span-1 col-span-2'}>
-                    <h2 className={'md:text-8xl text-5xl font-bold text-amber-900'}>Compra Chocolates Goyito</h2>
-                    <p className={'md:text-xl text-xs font-sans  md:mt-10 mt-5'}>
+                <div className="md:ml-20 ml-0 md:col-span-1 col-span-2">
+                    <h2 className="md:text-8xl text-5xl font-bold text-amber-900">Compra Chocolates Goyito</h2>
+                    <p className="md:text-xl text-xs font-sans md:mt-10 mt-5">
                         Completa el formulario y nos pondremos en contacto contigo en la brevedad.
                     </p>
                     {isSubmitted && <div
@@ -110,46 +110,46 @@ const Form = () => {
                         <span className="block sm:inline"> Tu solicitud ha sido enviada correctamente.</span>
                     </div>}
                     <form onSubmit={handleSubmit(onSubmit)}
-                          className={'p-10 md:mt-10 mt-5 bg-white border border-gray-200 rounded-lg shadow w-full'}>
-                        <div className={'w-full'}>
+                          className="p-10 md:mt-10 mt-5 bg-white border border-gray-200 rounded-lg shadow-xl space-y-6">
+                        <div className="w-full">
                             <label htmlFor="fullName" className="block text-xl font-medium text-gray-700">Nombre
                                 Completo</label>
                             <input type="text" {...register('fullName')} id="fullName"
                                    className="mt-1 focus:ring-amber-600 focus:border-amber-600 block w-full shadow-sm sm:text-sm border-gray-300 border rounded"/>
                             {errors.fullName && <p className="text-red-500">{errors.fullName.message}</p>}
                         </div>
-                        <div className="mt-6 w-full">
+                        <div className="w-full">
                             <label htmlFor="email" className="block text-xl font-medium text-gray-700">Correo</label>
                             <input type="email" {...register('email')} id="email" autoComplete="email"
                                    className="mt-1 focus:ring-amber-600 focus:border-amber-600 block w-full shadow-sm sm:text-sm border-gray-300 border rounded"/>
                             {errors.email && <p className="text-red-500">{errors.email.message}</p>}
                         </div>
-                        <div className={'mt-6 w-full'}>
+                        <div className="w-full">
                             <label htmlFor="location"
                                    className="block text-xl font-medium text-gray-700">Dirección</label>
                             <input type="text" {...register('location')} id="location" autoComplete="location"
                                    className="mt-1 focus:ring-amber-600 focus:border-amber-600 block w-full shadow-sm sm:text-sm border-gray-300 border rounded" />
                             {errors.location && <p className="text-red-500">{errors.location.message}</p>}
                         </div>
-                        <div className={'md:mt-6 mt-3 flex md:flex-row flex-col justify-between w-full'}>
-                            <div>
-                                <label htmlFor="phone" className="block text-xl font-medium text-gray-700">Telefono</label>
+                        <div className="flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0 w-full">
+                            <div className="w-full">
+                                <label htmlFor="phone" className="block text-xl font-medium text-gray-700">Teléfono</label>
                                 <input type="tel" placeholder="999-999-999" {...register('phone')} pattern="[0-9]{9}" maxLength={9} id="phone" autoComplete="phone" className="mt-1 focus:ring-amber-600 focus:border-amber-600 block w-full shadow-sm sm:text-sm border-gray-300 border rounded" />
                                 {errors.phone && <p className="text-red-500">{errors.phone.message}</p>}
                             </div>
-                            <div>
+                            <div className="w-full">
                                 <label htmlFor="date" className="block text-xl font-medium text-gray-700">Fecha</label>
                                 <input type="date" {...register('date')} id="date" autoComplete="date" className="mt-1 focus:ring-amber-600 focus:border-amber-600 block w-full shadow-sm sm:text-sm border-gray-300 border rounded" />
                                 {errors.date && <p className="text-red-500">{errors.date.message}</p>}
                             </div>
-                            <div className={'md:mt-0 mt-6'}>
+                            <div className="w-full">
                                 <label htmlFor="time" className="block text-xl font-medium text-gray-700">Hora</label>
                                 <input type="time" {...register('time')} id="time" autoComplete="time" className="mt-1 focus:ring-amber-600 focus:border-amber-600 block w-full shadow-sm sm:text-sm border-gray-300 border rounded" />
                                 {errors.time && <p className="text-red-500">{errors.time.message}</p>}
                             </div>
                         </div>
-                        <div className={'md:mt-6 mt-3 flex md:flex-row flex-col justify-between w-full'}>
-                            <div className={'md:mt-0 mt-6'}>
+                        <div className="flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0 w-full">
+                            <div className="w-full">
                                 <label htmlFor="typeOfChocolate" className="block text-xl font-medium text-gray-700">Tipo de Chocolate</label>
                                 <select {...register('typeOfChocolate')} id="typeOfChocolate" autoComplete="typeOfChocolate" className="mt-1 focus:ring-amber-600 focus:border-amber-600 block w-full shadow-sm sm:text-sm border-gray-300 border rounded">
                                     <option value="Seleccionar">Seleccionar</option>
@@ -159,7 +159,7 @@ const Form = () => {
                                 </select>
                                 {errors.typeOfChocolate && <p className="text-red-500">{errors.typeOfChocolate.message}</p>}
                             </div>
-                            <div className={'md:mt-0 mt-6'}>
+                            <div className="w-full">
                                 <label htmlFor="quantityOfChocolate" className="block text-xl font-medium text-gray-700">Cantidad</label>
                                 <select {...register('quantityOfChocolate')} id="quantityOfChocolate" autoComplete="quantityOfChocolate" className="mt-1 focus:ring-amber-600 focus:border-amber-600 block w-full shadow-sm sm:text-sm border-gray-300 border rounded">
                                     <option value="Seleccionar">Seleccionar</option>
